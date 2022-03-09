584e2ebd995ca575a4a4ffec02dd45166c87dcba
Luka Kureljušić <lkureljusic@ymail.com>
2022-03-09 21:12:57 +0100

    fix static content share

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,8 @@ import { ReactionModule } from './reaction/reaction.module';
 import { CommentModule } from './comment/comment.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { MailModule } from './helpers/mail/mail.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 
 @Module({
@@ -20,8 +22,19 @@ import { MailModule } from './helpers/mail/mail.module';
     PrismaModule,
     MailModule,
     ConfigModule.forRoot({isGlobal:true}),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'data', 'users'),
+      serveRoot: '/data/users/',
+      exclude: ['/api*'],      
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'data', 'posts'),
+      serveRoot: '/data/posts/',
+      exclude: ['/api*'],      
+    }),
   ],
   controllers: [],
   providers: [],
 })
+
 export class AppModule { }
