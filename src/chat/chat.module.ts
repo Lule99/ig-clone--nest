01fd4da70d30d04ac464534ea9161008ad878e9b
Luka Kureljušić <lkureljusic@ymail.com>
2022-03-10 08:24:08 -0800

    import

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ChatGateway } from './gateways/chat.gateway';
+import { GlobalGateway } from './gateways/global.gateway';
 
 @Module({
-  providers: [ChatGateway]
+  providers: [ChatGateway, GlobalGateway]
 })
 export class ChatModule {}
