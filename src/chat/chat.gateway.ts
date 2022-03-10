a91f67690b3f660afce3b9a4c0f5a514c362b0a2
Luka Kureljušić <lkureljusic@ymail.com>
2022-03-10 05:37:23 -0800

    chatToServer

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -5,12 +5,18 @@ import {
   WebSocketServer,
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
+import { PrismaService } from 'src/prisma/prisma.service';
 
-@WebSocketGateway({ namespace: '/chat', cors: {
-  origin: "http://localhost:3000",
-  credentials: true
-}})
+@WebSocketGateway({
+  namespace: '/chat',
+  cors: {
+    origin: 'http://localhost:3000',
+    credentials: true,
+  },
+})
 export class ChatGateway implements OnGatewayInit {
+  constructor(private prisma: PrismaService) {}
+
   @WebSocketServer() wss: Server;
 
   afterInit(server: any) {
@@ -22,7 +28,11 @@ export class ChatGateway implements OnGatewayInit {
     client: Socket,
     message: { sender: string; room: string; message: string },
   ) {
-    this.wss.to(message.room).emit('chatToClient', message);
+    this.getUserFromUsername(message.sender).then((res) => {
+      const { username, profilePicture } = res;
+      const msg = { username, profilePicture, text: message.message, dateTime: new Date() };
+      this.wss.to(message.room).emit('chatToClient', msg);
+    });
   }
 
   @SubscribeMessage('joinRoom')
@@ -35,5 +45,17 @@ export class ChatGateway implements OnGatewayInit {
   handleRoomLeave(client: Socket, room: string) {
     client.leave(room);
     client.emit('leftRoom', room);
+  }
+
+  async getUserFromUsername(username: string) {
+    const profile = await this.prisma.user
+      .findFirst({
+        where: {
+          username: username,
+        },
+      })
+      .profile();
+
+    return { username, profilePicture: profile.profilePicture };
   }
 }
