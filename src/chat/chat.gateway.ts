82eefcd239e30b62eadbe99b8382aa0839137dca
Luka Kureljušić <lkureljusic@ymail.com>
2022-03-10 04:15:48 -0800

    cors

---
+++ src/chat/chat.gateway.ts
@@ -0,0 +1,29 @@
+import { OnGatewayInit, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+
+@WebSocketGateway({namespace:'/chat', cors:{origin:'/*'}})
+export class ChatGateway implements OnGatewayInit {
+
+  @WebSocketServer() wss: Server;
+
+  afterInit(server: any) {
+    //ovde potencijalno ocistiti sve sobe?
+  }
+
+  @SubscribeMessage('chatToServer')
+  handleMessage(client: Socket, message: { sender: string, room: string, message: string }) {
+    this.wss.to(message.room).emit('chatToClient', message);
+  }
+
+  @SubscribeMessage('joinRoom')
+  handleRoomJoin(client: Socket, room: string ) {
+    client.join(room);
+    client.emit('joinedRoom', room);
+  }
+
+  @SubscribeMessage('leaveRoom')
+  handleRoomLeave(client: Socket, room: string ) {
+    client.leave(room);
+    client.emit('leftRoom', room);
+  }
+}
